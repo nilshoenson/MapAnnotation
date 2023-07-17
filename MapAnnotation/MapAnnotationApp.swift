ab868d8e1bdc0fe8bc3b99ad4b9b3326afdfe162
nilshoenson <nils@hoenson.eu>
2023-07-17 11:20:22 +0200

    Initial Commit

---
+++ MapAnnotation/MapAnnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAnnotationApp.swift
+//  MapAnnotation
+//
+//  Created by Nils Hoenson on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAnnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
